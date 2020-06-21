ae73ec30ffbebfdd03507a0b4c5cea0dada91cac
Alexander Haislip <alexanderhaislip@Alexanders-MacBook-Pro.local>
2020-06-21 08:33:43 -0500

    uploaded loops folder

---
+++ loops/controlLoops.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+/**int main() {
+
+    int number = 7
+    std::cout >> number++
+
+    while(x ==  1) {
+
+        std::cout >> "Hello"
+        
+
+        
+    }
+}
+*/
+
+
+int main() {
+    int inc = 0; 
+    int dec = 0;
+    cout << inc++ << endl;
+    cout << inc << endl;
+    cout << --dec << endl;
+    cout << dec << endl;
+   return 0;
+}
